60540a30e4be23a3aecfb3e6037d1d3a32d91469
studiomic <sakurai@studiomic.net>
2022-05-12 03:52:22 +0900

    Githubb版に差し替え

--- includes/Scripts.php
+++ includes/Scripts.php
@@ -21,23 +21,7 @@ class Scripts {
 	 * @since 1.0
 	 */
 	public function __construct() {
-		add_action( 'wp_head', [ $this, 'remove_embed_script' ] );
 		add_action( 'wp_footer', [ $this, 'footer' ] );
-	}
-
-	/**
-	 * Removes the embed script if there are no iframes.
-	 *
-	 * @access public
-	 * @since 0.6.3
-	 * @return void
-	 */
-	public function remove_embed_script() {
-		global $template_html;
-
-		if ( $template_html && ! strpos( $template_html, '<iframe' ) ) {
-			wp_dequeue_script( 'wp-embed' );
-		}
 	}
 
 	/**
@@ -48,7 +32,7 @@ class Scripts {
 	 */
 	public function footer() {
 		echo '<script>';
-		include get_theme_file_path( 'scripts/prefetch-on-hover.js' );
+		include get_theme_file_path( 'assets/scripts/prefetch-on-hover.js' );
 		echo '</script>';
 	}
 }
